6733baa54ed2b00bf417e8b85753d4433d631ae0
DavidOnak <david.onak@shaw.ca>
2020-10-18 22:41:37 -0700

    Improved buttons and added connection to messages

--- Front_end/app/src/main/java/com/example/tunnect/MainActivity.java
+++ Front_end/app/src/main/java/com/example/tunnect/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.tunnect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button messagesBtn, profileBtn, settingsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        messagesBtn = findViewById(R.id.messages_btn);
+        messagesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mapIntent = new Intent(MainActivity.this, MessagesActivity.class);
+                startActivity(mapIntent);
+            }
+        });
     }
 }
