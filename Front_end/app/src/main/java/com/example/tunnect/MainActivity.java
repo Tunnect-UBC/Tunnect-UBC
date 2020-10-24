fff54a57c950b0d521fc25931350cd7e6d77bdff
DavidOnak <david.onak@shaw.ca>
2020-10-24 16:04:31 -0700

    Can now recieve push notifications, foreground and background

--- Front_end/app/src/main/java/com/example/tunnect/MainActivity.java
+++ Front_end/app/src/main/java/com/example/tunnect/MainActivity.java
@@ -1,27 +1,42 @@
 package com.example.tunnect;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button messagesBtn, profileBtn, settingsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        messagesBtn = findViewById(R.id.messages_btn);
+        Button messagesBtn = findViewById(R.id.messages_btn);
         messagesBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent mapIntent = new Intent(MainActivity.this, MessagesActivity.class);
-                startActivity(mapIntent);
+                Intent messageIntent = new Intent(MainActivity.this, MessagesActivity.class);
+                startActivity(messageIntent);
+            }
+        });
+
+        Button profileBtn = findViewById(R.id.profile_btn);
+        profileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent profileIntent = new Intent(MainActivity.this, ProfileActivity.class);
+                //startActivity(profileIntent);
+            }
+        });
+
+        Button settingsBtn = findViewById(R.id.settings_btn);
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
+                //startActivity(settingsIntent);
             }
         });
     }
--- Front_end/app/src/main/java/com/example/tunnect/MessagesActivity.java
+++ Front_end/app/src/main/java/com/example/tunnect/MessagesActivity.java
@@ -1,14 +1,87 @@
 package com.example.tunnect;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+import java.util.Objects;
 
 public class MessagesActivity extends AppCompatActivity {
+    private BroadcastReceiver messageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(@Nullable Context context, @NonNull Intent intent) {
+            //text_view_notification.text = intent.getExtras().getString("message");
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
+
+        // Must first check if device can send and receive messages
+        if (MessagesActivity.this.checkGooglePlayServices()) {
+            FirebaseInstanceId id = FirebaseInstanceId.getInstance();
+            id.getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                @Override
+                public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                    if (!task.isSuccessful()) {
+                        Log.w("user", "getInstanceId failed", task.getException());
+                        return;
+                    }
+
+                    // To know if connection works, we want to see the resulting token
+                    String token = Objects.requireNonNull(task.getResult()).getToken();
+                    Toast.makeText(getApplicationContext(), token, Toast.LENGTH_LONG).show();
+                    Log.d("user", token);
+                }
+            });
+        } else {
+            Toast.makeText(getApplicationContext(), "This device cannot receive notifications! Must have google play services.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    /*
+     *   This function checks Google Play services to see if the device can receive notifications.
+     *  @return: returns true if it can receive notifications, false otherwise.
+     */
+    private boolean checkGooglePlayServices() {
+        int status = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable((Context)this);
+        if (status != ConnectionResult.SUCCESS) {
+            Log.e("MainActivity", "Error");
+            return false;
+        } else {
+            Log.i("MainActivity", "Google play services updated");
+            return true;
+        }
+    }
+
+    protected void onStart() {
+        super.onStart();
+        LocalBroadcastManager.getInstance(this).registerReceiver(messageReceiver, new IntentFilter("MyData"));
+    }
+
+    protected void onStop() {
+        super.onStop();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(messageReceiver);
     }
 }
