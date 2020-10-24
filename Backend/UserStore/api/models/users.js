956d20b58f7c102a8d4a063729e12aa269bb3d07
nickhamiltonn <nick.hamilton@shaw.ca>
2020-10-24 13:59:54 -0700

    Added .gitignore as well as deleted unused files

--- Backend/UserStore/api/models/users.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = mongoose.Schema({
-    _id: String,
-    username: String,
-    top_artist: String
-});
-
-module.exports = mongoose.model('User', userSchema);
