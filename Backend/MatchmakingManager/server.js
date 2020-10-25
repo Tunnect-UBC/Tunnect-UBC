33915a709c58475e84df13b0179a9b5898731d1b
nickhamiltonn <nick.hamilton@shaw.ca>
2020-10-24 19:46:37 -0700

    Basic implementation for matchmaker

---
+++ Backend/MatchmakingManager/server.js
@@ -0,0 +1,14 @@
+/**
+ * This is the first file that is run when you run "npm run-script start-matchmaker"
+ * 
+ * This listens on port 3001 by default, and creates the server that is specified in app.js
+ * 
+ */
+const http = require('http');
+const app = require('./app');
+
+const port = process.env.PORT || 3001;
+
+const server = http.createServer(app);
+
+server.listen(port, '0.0.0.0');
--- Backend/models/users.js
+++ Backend/models/users.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     _id: String,
     username: String,
-    top_artist: String
+    top_artist: String,
 });
 
 module.exports = mongoose.model('User', userSchema);
