c25607641e57c898fa5b3e1dbca2a1c50de29d8b
Lance Holland <lanceholland01@gmail.com>
2020-10-24 18:20:40 -0600

    Basic search functionality

--- Front_end/app/src/main/java/com/example/tunnect/MainActivity.java
+++ Front_end/app/src/main/java/com/example/tunnect/MainActivity.java
@@ -2,13 +2,100 @@ package com.example.tunnect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    // UI definitions
+    private TextView userView;
+    private TextView song_album;
+    private TextView song_title;
+    private TextView song_artist;
+    private Button addBtn;
+    private Song song;
+    private Button search_button;
+    private EditText search_bar;
+    private Button next_song_button;
+
+    // Song service used to access spotify
+    private SongService songService;
+    // Songs returned from a search
+    private ArrayList<Song> search_songs;
+    // Value used to track what song is currently displayed
+    private int current_song;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        songService = new SongService(getApplicationContext());
+        userView = (TextView) findViewById(R.id.user);
+        song_title = (TextView) findViewById(R.id.song_title);
+        song_album = (TextView) findViewById(R.id.song_album);
+        song_artist = (TextView) findViewById(R.id.song_artist);
+        addBtn = (Button) findViewById(R.id.add);
+        search_bar = (EditText) findViewById(R.id.search_bar);
+        next_song_button = (Button) findViewById(R.id.next_song_button);
+        search_button = (Button) findViewById(R.id.search_button);
+
+        // Set the user id to display
+        SharedPreferences sharedPreferences = this.getSharedPreferences("SPOTIFY", 0);
+        userView.setText(sharedPreferences.getString("userid", "No User"));
+
+        // Search button functionality
+        search_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                songService.set_URL(search_bar.getText().toString());
+                songService.search(() -> {
+                    search_songs = songService.get_search_songs();
+                });
+                current_song = 0;
+                if (search_songs == null) {
+                    song_title.setText("Search");
+                    song_album.setText("Failed");
+                    song_artist.setText(":(");
+                }
+                else {
+                    dispSong(search_songs.get(current_song));
+                }
+            }
+        });
+
+        // Next song button functionality
+        next_song_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateSong();
+            }
+        });
     }
+
+    // Updates the currently displayed song
+    private void updateSong() {
+        current_song++;
+        if (current_song < search_songs.size()) {
+            dispSong(search_songs.get(current_song));
+        }
+        else {
+            current_song = 0;
+            dispSong(search_songs.get(current_song));
+        }
+    }
+
+    // Displays the info of the given song
+    private void dispSong(Song song) {
+        song_title.setText(song.getName());
+        song_album.setText(song.getAlbum());
+        song_artist.setText(song.getArtist());
+    }
+
 }
---
+++ Front_end/app/src/main/java/com/example/tunnect/SplashActivity.java
@@ -0,0 +1,152 @@
+package com.example.tunnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SplashActivity extends AppCompatActivity {
+    private SharedPreferences.Editor editor;
+    private SharedPreferences msharedPreferences;
+
+    private RequestQueue queue;
+
+
+    private static final String CLIENT_ID = "b30cb6a307474da78191b84e475f90a6";
+    private static final String REDIRECT_URI = "com.Tunnect://callback";
+    private static final int REQUEST_CODE = 1337;
+    private static final String SCOPES = "user-read-recently-played,user-library-modify,user-read-email,user-read-private";
+
+    // This activity starts when the app opens and checks spotify permissions and if we don't have them it requests them
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash);
+
+        authenticateSpotify();
+
+        msharedPreferences = this.getSharedPreferences("SPOTIFY", 0);
+        queue = Volley.newRequestQueue(this);
+    }
+    private void authenticateSpotify() {
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+        builder.setScopes(new String[]{SCOPES});
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    editor = getSharedPreferences("SPOTIFY", 0).edit();
+                    editor.putString("token", response.getAccessToken());
+                    Log.d("STARTING", "GOT AUTH TOKEN");
+                    Log.i("fucker", "fuck yeah");
+                    editor.apply();
+                    waitForUserInfo();
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    // Handle error response
+                    Log.i("fucker", response.getError());
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    // Handle other cases
+            }
+        }
+    }
+    private void waitForUserInfo() {
+        UserService userService = new UserService(queue, msharedPreferences);
+        userService.get(() -> {
+            User user = userService.getUser();
+            editor = getSharedPreferences("SPOTIFY", 0).edit();
+            editor.putString("userid", user.id);
+            Log.d("STARTING", "GOT USER INFORMATION");
+            // We use commit instead of apply because we need the information stored immediately
+            editor.commit();
+            startMainActivity();
+        });
+    }
+    // Transfers to the main activity
+    private void startMainActivity() {
+        Intent newintent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(newintent);
+    }
+}
+
+// User class (barebones right now)
+class User {
+
+    public String id;
+}
+
+// UserService that authorizes the user with spotify
+class UserService {
+
+    private static final String ENDPOINT = "https://api.spotify.com/v1/me";
+    private SharedPreferences msharedPreferences;
+    private RequestQueue mqueue;
+    private User user;
+
+    public UserService(RequestQueue queue, SharedPreferences sharedPreferences) {
+        mqueue = queue;
+        msharedPreferences = sharedPreferences;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void get(final VolleyCallBack callBack) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(ENDPOINT, null, response -> {
+            Gson gson = new Gson();
+            user = gson.fromJson(response.toString(), User.class);
+            callBack.onSuccess();
+        }, error -> get(() -> {
+
+        })) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                String token = msharedPreferences.getString("token", "");
+                String auth = "Bearer " + token;
+                headers.put("Authorization", auth);
+                return headers;
+            }
+        };
+        mqueue.add(jsonObjectRequest);
+    }
+}
+
+interface VolleyCallBack {
+
+    void onSuccess();
+}
+
