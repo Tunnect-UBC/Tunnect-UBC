aad802dd70c8f9023a7ba7637d858f6a03675b95
Ubuntu <tunnect@Tunnect.fme2xm4a4ngezhuj2cktviyq0g.bx.internal.cloudapp.net>
2020-10-26 17:25:23 +0000

    chat updated

---
+++ Backend/models/chat.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const ChatSchema = new Schema({
+  usrID1 : String,
+  usrID2 : String,
+  messages :  [{sender_name: String, message: String}],
+  lastmessage : String
+});
+
+const Chat = mongoose.model('chat', ChatSchema);
+module.exports = Chat;
