9c9656c7a0284d996c8c697e57c6482d908683f5
DavidOnak <david.onak@shaw.ca>
2020-10-26 04:10:39 -0700

    Set up messages connection with server

--- Front_end/app/src/main/java/com/example/tunnect/MessageListActivity.java
+++ Front_end/app/src/main/java/com/example/tunnect/MessageListActivity.java
@@ -30,7 +30,8 @@ import java.util.List;
  */
 public class MessageListActivity extends AppCompatActivity {
 
-    private static final String BASE_URL = "http://35.236.81.110/time";
+    private static final String testID = "35i4h34h5j69jk";
+    private static final String BASE_URL = "http://52.188.167.58:5000/chatservice/"+testID;
     private RequestQueue queue;
     RecyclerView chatOptions;
     ChatListAdaptor chatListAdaptor;
@@ -77,37 +78,30 @@ public class MessageListActivity extends AppCompatActivity {
     // Using a Volley connection, this method adds entries in the chatsList from the provided server data
     private void populateChatList() {
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, BASE_URL, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        JSONArray jsonArray;
-                        try {
-                            jsonArray = response.getJSONArray("chats");
+                response -> {
+                    JSONArray jsonArray;
+                    try {
+                        jsonArray = response.getJSONArray("availChats");
 
-                            for (int i = 0; i < jsonArray.length(); i++) {
-                                JSONObject chat = jsonArray.getJSONObject(i);
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject chat = jsonArray.getJSONObject(i);
 
-                                chatsList.add(new Chat(chat.getString("Id"), chat.getString("Name"),
-                                        chat.getString("LastMessage"), chat.getString("Timestamp"), chat.getInt("Colour")));
-
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                            Toast.makeText(getApplicationContext(), "Failed to retrieve data from server!", Toast.LENGTH_LONG).show();
+                            //chatsList.add(new Chat(chat.getString("user_id"), chat.getString("user_colour"),
+                                    //chat.getString("last_message"), chat.getString("Timestamp"), chat.getInt("Colour")));
+                            chatsList.add(new Chat(chat.getString("user_id"), chat.getString("user_name"),
+                                    chat.getString("last_message"), "12:69am", chat.getInt("user_colour")));
                         }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(getApplicationContext(), "Failed to retrieve data from server!", Toast.LENGTH_LONG).show();
                     }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getApplicationContext(), "Connection to server failed", Toast.LENGTH_LONG).show();
-            }
-        });
+                }, error -> Toast.makeText(getApplicationContext(), "Connection to server failed", Toast.LENGTH_LONG).show());
 
         queue.add(request);
 
         // these entries are added for testing purposes
         //TODO: Delete this when testing is done!!!!!!!!!!!!!!!!!
-        chatsList.add(new Chat("0", "David Onak", "That's sus man!", "10:33am", 0xFF44AA44));
+        chatsList.add(new Chat(testID, "David Onak", "That's sus man!", "10:33am", 0xFF44AA44));
         chatsList.add(new Chat("1", "Jeff", "My name is Jeff", "8:00am", 0xFF4444AA));
         chatsList.add(new Chat("2", "Nick Hamilton", "Your a beast!", "Oct. 24", 0xFFAA4444));
         chatsList.add(new Chat("3", "Joe Smith", "Hello, I am Linsay Lohan!", "Oct. 23", 0xFF222222));
