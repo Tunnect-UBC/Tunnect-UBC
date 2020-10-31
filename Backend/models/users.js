8abddb0dc894f74816925f7afd9c1e4f8a80f40c
nickhamiltonn <nick.hamilton@shaw.ca>
2020-10-30 19:20:30 -0700

    Fixed matchmaker and userstore, comments still need to be updated

--- Backend/models/users.js
+++ Backend/models/users.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
+/**
+ * Adding song array... hasnt been compiled or tested...
+ */
 const userSchema = mongoose.Schema({
     _id: String,
     username: String,
     top_artist: String,
-    icon_colour: String
+    icon_colour: String,
+    songs: Array
 });
 
 module.exports = mongoose.model('User', userSchema);
