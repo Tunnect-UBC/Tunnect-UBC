46060f03ef3954c87bf2a0f49e34fcc15cd11be8
DavidOnak <david.onak@shaw.ca>
2020-11-07 20:41:43 -0800

    Make sick improvment in profile activity with the keyboard

--- Front_end/app/src/main/java/com/example/tunnect/ProfileActivity.java
+++ Front_end/app/src/main/java/com/example/tunnect/ProfileActivity.java
@@ -6,11 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.graphics.drawable.DrawableCompat;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
