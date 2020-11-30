15306c169f2c603d7a6a7f7d11b2a20b3900b39a
elisehdy <54967815+elisehdy@users.noreply.github.com>
2020-11-29 21:14:04 -0800

    fix merge conflict

--- Backend/models/users.js
+++ Backend/models/users.js
@@ -12,22 +12,20 @@ const song = {
 const userSchema = new mongoose.Schema({
     _id: String,
     username: String,
-<<<<<<< HEAD
     topArtist: String,
     iconColour: String,
     notifId: String,
     songs: Array,
     matches: Array,
     likes: Array,
-    dislikes: Array
-=======
+    dislikes: Array,
     favGenre: String,
     iconColour: Number,
     songs: [song],
     matches: [String],
     likes: [String],
     dislikes: [String]
->>>>>>> 61cabce71537161a62e4bb7f541d6b8528fb1b00
+
 });
 
 module.exports = mongoose.model("User", userSchema);
