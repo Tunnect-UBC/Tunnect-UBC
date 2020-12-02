ea556dee59ce1785c276980de86f30e559c1a605
DavidOnak <david.onak@shaw.ca>
2020-12-01 23:29:39 -0800

    Wrote code for removing user from matches and chats

--- Front_end/app/src/main/java/com/example/tunnect/SettingsActivity.java
+++ Front_end/app/src/main/java/com/example/tunnect/SettingsActivity.java
@@ -2,7 +2,10 @@ package com.example.tunnect;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.drawable.DrawableCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,20 +14,30 @@ import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class SettingsActivity extends AppCompatActivity {
     private String USER_ID;
     private final String DELETE_URL = "http://52.188.167.58:3000/userstore/";
+    private static final String SC_URL = "http://52.188.167.58:5000/chatservice/";
+
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         USER_ID = Objects.requireNonNull(getIntent().getExtras()).getString("USER_ID");
+        queue = Volley.newRequestQueue(this);
 
         // Start by setting up a title for the page
         ActionBar actionBar = getSupportActionBar();
@@ -36,20 +49,102 @@ public class SettingsActivity extends AppCompatActivity {
         // Button to delete current user's account
         Button deleteBtn = findViewById(R.id.delete_account);
         deleteBtn.setOnClickListener(view -> {
-            deleteUser();
+            deleteChats();
         });
     }
 
-    // TODO: Have this function return to the SplashActivity
-    public void deleteUser() {
-        RequestQueue queue = Volley.newRequestQueue(this);
-
+    // Deletes the account information for a user
+    public void deleteUserAccount() {
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.DELETE, DELETE_URL+USER_ID, null, response -> {
             Toast.makeText(getApplicationContext(), "Your account has been deleted!", Toast.LENGTH_LONG).show();
+            Intent profileIntent = new Intent(SettingsActivity.this, SplashActivity.class);
+            startActivity(profileIntent);
         }, error -> {
             Toast.makeText(getApplicationContext(), "Failed to delete account!", Toast.LENGTH_LONG).show();
         });
         queue.add(jsonObjectRequest);
+    }
+
+    // Deletes all existing chats with this user, then calls deleteMatches
+    private void deleteChats() {
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, SC_URL+USER_ID, null,
+                response -> {
+                    try {
+                        JsonObjectRequest jsonObjectRequest;
+                        for (int i = 0; i < response.length(); i++) {
+                            JSONObject chat = response.getJSONObject(i);
+                            if (chat.has("usrID1")) {
+                                jsonObjectRequest = new JsonObjectRequest(Request.Method.DELETE, SC_URL+chat.getString("usrID1")+"/"+USER_ID, null, result -> {
+                                    Toast.makeText(getApplicationContext(), "Your chats have been deleted!", Toast.LENGTH_LONG).show();
+                                }, error -> {
+                                    Toast.makeText(getApplicationContext(), "Failed to delete chats!", Toast.LENGTH_LONG).show();
+                                });
+                            } else {
+                                jsonObjectRequest = new JsonObjectRequest(Request.Method.DELETE, SC_URL+USER_ID+"/"+chat.getString("usrID2"), null, result -> {
+                                    Toast.makeText(getApplicationContext(), "Your chats have been deleted!", Toast.LENGTH_LONG).show();
+                                }, error -> {
+                                    Toast.makeText(getApplicationContext(), "Failed to delete chats!", Toast.LENGTH_LONG).show();
+                                });
+                            }
+                            queue.add(jsonObjectRequest);
+                        }
+                        deleteMatches();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(getApplicationContext(), "Failed to retrieve data from server!", Toast.LENGTH_LONG).show();
+                    }
+                }, error -> Toast.makeText(getApplicationContext(), "Connection to server failed", Toast.LENGTH_LONG).show());
+        queue.add(request);
+    }
+
+    // Deletes all existing chats with this user, then calls deleteUserAccount
+    private void deleteMatches() {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, DELETE_URL+USER_ID, null,
+                response -> {
+                    try {
+                        JSONArray jsonMatches = response.optJSONArray("matches");
+                        JsonObjectRequest jsonObjectRequest;
+
+                        for (int i = 0; i < jsonMatches.length(); i++) {
+                            String userID = jsonMatches.getString(i);
+                            jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, DELETE_URL+userID, null, result -> {
+                                try {
+                                    // Patch existing match list for other user
+                                    JSONArray jsonOtherMatches = result.optJSONArray("matches");
+                                    ArrayList<String> newMatches = new ArrayList<>();
+                                    for (int j = 0; j < jsonOtherMatches.length(); j++) {
+                                        JSONArray patchArray = new JSONArray();
+
+                                        if (!jsonOtherMatches.getString(j).equals(USER_ID)) {
+                                            newMatches.add(jsonOtherMatches.getString(j));
+                                        }
+
+                                        JSONObject songObject = new JSONObject();
+                                        songObject.put("propName", "matches");
+                                        songObject.put("value", newMatches);
+                                        patchArray.put(songObject);
+                                        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.PATCH, DELETE_URL+userID, patchArray, r -> {
+                                        }, error -> {
+                                            // TODO: This adds the song correctly but it returns error for some reason, check it with Nick
+                                            Toast.makeText(getApplicationContext(), "Failed to patch match!", Toast.LENGTH_LONG).show();
+                                        });
+                                        queue.add(jsonArrayRequest);
+                                    }
+                                } catch (JSONException e) {
+                                    Toast.makeText(getApplicationContext(), "Failed to delete match!", Toast.LENGTH_LONG).show();
+                                }
+                            }, error -> {
+                                Toast.makeText(getApplicationContext(), "Failed to delete match!", Toast.LENGTH_LONG).show();
+                            });
+                            queue.add(jsonObjectRequest);
+                        }
+
+                        deleteUserAccount();
+                    } catch (JSONException e) {
+                        Toast.makeText(getApplicationContext(), "Failed to retrieve data from server!", Toast.LENGTH_LONG).show();
+                    }
+                }, error -> Toast.makeText(getApplicationContext(), "Connection to server failed", Toast.LENGTH_LONG).show());
+        queue.add(request);
     }
 
     // Code to return to last page when the return button on the title bar is hit
