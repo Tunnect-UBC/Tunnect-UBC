23e286a09b242d43de8161e2e126ac1f82364dce
elisehdy <54967815+elisehdy@users.noreply.github.com>
2020-12-02 22:28:15 -0800

    codecy

--- Backend/__tests__/score.tests.js
+++ Backend/__tests__/score.tests.js
@@ -17,7 +17,7 @@ describe("Test for score #2, user1.songs.length == 0, genreScore == 0", () => {
     it("Calling score test #2", () => {
         helpers.songScore = jest.fn((song, user2) => {
             return 0;
-        })
+        });
         let score = helpers.getScore(userDictionary.user2, userDictionary.user1);
         expect(score).toEqual(0);
     });
@@ -32,7 +32,7 @@ describe("Test for score #3, user1.songs.length != 0, genreScore == 3", () => {
             } else {
                 return 0;
             }
-        })
+        });
         let score = helpers.getScore(userDictionary.user3, userDictionary.user1);
         expect(score).toEqual(10);
     });
@@ -43,7 +43,7 @@ describe("Test for score #4, user1.songs.length == 0, genreScore == 3", () => {
     it("Calling score test #4", () => {
         helpers.songScore = jest.fn((song, user2) => {
             return 0;
-        })
+        });
         let score = helpers.getScore(userDictionary.user2, userDictionary.user4);
         expect(score).toEqual(3);
     });
