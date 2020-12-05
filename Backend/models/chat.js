0c1af2affbb03741d8a24fad771b830746b6d3a0
elisehdy <54967815+elisehdy@users.noreply.github.com>
2020-12-04 21:46:40 -0800

    number icon coulour

--- Backend/models/chat.js
+++ Backend/models/chat.js
@@ -4,10 +4,10 @@ const Schema = mongoose.Schema;
 
 const ChatSchema = new Schema({
   usrID1 : String,
-  usrColour1: String,
+  usrColour1: Number,
   usrName1: String,
   usrID2 : String,
-  usrColour2: String,
+  usrColour2: Number,
   usrName2: String,
   messages :  [{senderid: String, message: String, timeStamp: Number}],
   lastMessage : String,
